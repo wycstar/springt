1ed5256faf4b863529af1acab0f4147d1ca2ff88
wyongchao <wyongchao@infervision.com>
2020-12-04 01:18:46 +0800

    [REG]202012040118

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,12 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.Bean.Login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
-@RequestMapping(path = "/session")
+@Controller
+@RequestMapping(path = "/user")
 public class LoginController {
-    // @PostMapping("/login")
-    
+    @GetMapping("/login")
+    public ModelAndView login(ModelAndView model){
+        model.setViewName("common/login.html");
+        return model;
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute Login login, RedirectAttributes ra) {
+        String username = login.getUsername();
+        String password = login.getPassword();
+
+        ra.addFlashAttribute("error", "yes");
+        return "redirect:/user/login";
+    }
 }
